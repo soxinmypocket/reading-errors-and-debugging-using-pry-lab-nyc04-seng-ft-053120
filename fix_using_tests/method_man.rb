4bbf06c93c4071566ee0ffe97a6e0e8c89906662
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-08 15:15:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
-def start_game(player1, player2)
+def start_game("Rocky", "Bullwinkle")
   "Hello #{player1} & #{playar2}"
-start_game("Rocky", "Bullwinkle")
 end
 
 def play_game(player2)
