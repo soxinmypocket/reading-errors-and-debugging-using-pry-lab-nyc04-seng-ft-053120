4f978e83b2af4a53e86cdde263b88c799ef62f46
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-08 15:23:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -7,7 +7,8 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
-  binding.pry
+def play_game(player1, player2)
+  player1 = "Jay-Z"
+  player2 = "Beyonce"
   return "#{player1} is better than #{player2}"
 end
